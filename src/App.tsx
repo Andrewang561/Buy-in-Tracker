692912c73b703c0224655983ad21e5dc2c66773b
andrewwang <andrewang561@hotmail.com>
2025-10-21 22:21:03 -0700

    Allowed dynamic inputs for player component.

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,48 @@ export interface Player {
 }
 
 function App() {
-  const temp: Player = {
+
+  const [playerValue, setPlayerValue] = useState<Player>({
     name: "Andrew",
     buyIn: 500,
     finalValue: 1000
-  }
+  });
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("Submitted player:", playerValue);
+  };
+
   return (
     <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={playerValue.name} onChange={(event) => 
+            setPlayerValue({ ...playerValue, name: event.target.value })
+          }></input>
+        </label>
+        <br />
+        <label>
+          Buy In:
+          <input type="number" value={playerValue.buyIn} onChange={(event) =>
+            setPlayerValue({ ...playerValue, buyIn: Number(event.target.value)})
+          }></input>
+        </label>
+        <br />
+        <label>
+          Final Value:
+          <input type= "number" value={playerValue.finalValue} onChange={(event) =>
+            setPlayerValue({ ...playerValue, finalValue: Number(event.target.value)})
+          }></input>
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
       <h1>Poker Tracker</h1>
-      <Players player1={temp} />
+      <Players player1={playerValue} />
     </div>
   );
 }
 
-export default App
+export default App;
